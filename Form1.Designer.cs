f0f88acb36769d2c5f7412cc0a51b9c847c3c6f1
Shahriar <mollahshahriar@outlook.com>
2020-01-08 19:27:36 -0500

    1st
    xcx

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace first_812020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDisplayOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(178, 125);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(232, 20);
+            this.txtAddress.TabIndex = 1;
+            this.txtAddress.Text = "123 Oak Street";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(392, 184);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(34, 20);
+            this.txtState.TabIndex = 5;
+            this.txtState.Text = "OH";
+            // 
+            // txtCity
+            // 
+            this.txtCity.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtCity.Location = new System.Drawing.Point(178, 184);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(114, 20);
+            this.txtCity.TabIndex = 4;
+            this.txtCity.Text = "Anytown";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(178, 78);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(232, 20);
+            this.txtName.TabIndex = 7;
+            this.txtName.Text = "Jane Holcar";
+            this.txtName.TextChanged += new System.EventHandler(this.TextBox8_TextChanged);
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(563, 184);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(100, 20);
+            this.txtZip.TabIndex = 9;
+            this.txtZip.Text = "5245";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(98, 295);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 10;
+            this.Button1.Text = "Display";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(587, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "NAME";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "City:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "State";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(500, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Zip:";
+            // 
+            // txtDisplayOutput
+            // 
+            this.txtDisplayOutput.Location = new System.Drawing.Point(75, 340);
+            this.txtDisplayOutput.Multiline = true;
+            this.txtDisplayOutput.Name = "txtDisplayOutput";
+            this.txtDisplayOutput.Size = new System.Drawing.Size(665, 162);
+            this.txtDisplayOutput.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.txtDisplayOutput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "Form1";
+            this.Text = "NAME";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDisplayOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_812020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox9_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String buffer;
+            buffer = "Mailing Label:" + Environment.NewLine + Environment.NewLine;
+            buffer = buffer + "Name: " + txtName.Text + Environment.NewLine;
+            buffer = buffer + "Address:" + txtAddress.Text + Environment.NewLine;
+            buffer = buffer + "City" + txtCity.Text +
+                                "State: " + txtState.Text + 
+                                " Zip: " + txtZip.Text;
+            txtDisplayOutput.Text = buffer;
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
