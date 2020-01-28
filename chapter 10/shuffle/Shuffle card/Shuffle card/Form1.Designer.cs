11e3f19abbd48cf641502e0b739f90f53fffa1c9
Shahriar <mollahshahriar@outlook.com>
2020-01-27 20:01:10 -0500

    shuffle

---
+++ chapter 10/shuffle/Shuffle card/Shuffle card/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Shuffle_card
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblPassCounter = new System.Windows.Forms.Label();
+            this.stDeck = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Location = new System.Drawing.Point(23, 13);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(75, 23);
+            this.btnShuffle.TabIndex = 0;
+            this.btnShuffle.Text = "Shuffle";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.BtnShuffle_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(135, 13);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(300, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // lblPassCounter
+            // 
+            this.lblPassCounter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPassCounter.Location = new System.Drawing.Point(23, 313);
+            this.lblPassCounter.Name = "lblPassCounter";
+            this.lblPassCounter.Size = new System.Drawing.Size(352, 23);
+            this.lblPassCounter.TabIndex = 4;
+            // 
+            // stDeck
+            // 
+            this.stDeck.FormattingEnabled = true;
+            this.stDeck.Location = new System.Drawing.Point(23, 45);
+            this.stDeck.Name = "stDeck";
+            this.stDeck.Size = new System.Drawing.Size(352, 251);
+            this.stDeck.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stDeck);
+            this.Controls.Add(this.lblPassCounter);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnShuffle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblPassCounter;
+        private System.Windows.Forms.ListBox stDeck;
+    }
+}
+
---
+++ chapter 10/shuffle/Shuffle card/Shuffle card/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shuffle_card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShuffle_Click(object sender, EventArgs e)
+        {
+            const int CARDSPERLINE = 13;
+            int j;
+            int cardIndex;
+            int deckSize;
+            int passes;
+            string buff;
+            string temp;
+            clsCardDeck myDeck = new clsCardDeck();
+            passes = myDeck.ShuffleDeck();
+            lblPassCounter.Text = "It took" + passes.ToString() + "passes to shuffle the deck";
+            deckSize = myDeck.DeckSize;
+            for (cardIndex = 1; cardIndex < deckSize + 1;)
+            {
+                buff = "";
+                for (j = 0; j < CARDSPERLINE; j++)
+
+                {
+                    temp = myDeck.getOneCard(cardIndex);
+                    if (temp.Length == 0)
+                    {
+                        MessageBox.Show("Error reading deck.", "Processing Error");
+                        return;
+                    }
+                    buff += temp + "";
+                    cardIndex++;
+                }
+                stDeck.Items.Add(buff);
+            }
+            stDeck.Items.Add("");
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            stDeck.Items.Clear();
+        }
+    }
+}
