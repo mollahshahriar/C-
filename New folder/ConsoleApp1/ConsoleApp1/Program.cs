2115b87f050d535167de0e749b474ddf624fb387
Shahriar <mollahshahriar@outlook.com>
2020-01-30 19:47:32 -0500

    ff

---
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamPracticeTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
+        }
+
+        private static bool MainMenu()
+        {
+            //Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Integers Value");
+            Console.WriteLine("2. Students Grade");
+            Console.WriteLine("3. Number of Digits");
+            Console.WriteLine("4. ASCII Codes");
+            Console.WriteLine("5. Spaces");
+            Console.WriteLine("6. Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    integer();
+                    return true;
+                case "2":
+                    studentsgrade();
+                    return true;
+                case "3":
+                    digits();
+                    return true;
+                case "4":
+                    ascii();
+                    return true;
+                case "5":
+                    spaces();
+                    return true;
+                case "6":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static void integer()
+        {
+            Console.Clear();
+            Console.WriteLine("Insert first value: ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert second value: ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert third value: ");
+            int value3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Numbers: " + " " + value1 + " " + value2 + " " + value3);
+            Console.WriteLine(" ");
+
+            if (value1 == value2 && value1 == value3)
+            {
+                Console.WriteLine("All the numbers are the same");
+            }
+            else if (value1 > value2 && value1 > value3)
+            {
+                Console.WriteLine("Greatest number is:" + value1);
+            }
+            else if (value2 > value1 && value2 > value3)
+            {
+                Console.WriteLine("Greatest number is:" + value2);
+            }
+            else
+            {
+                Console.WriteLine("Greatest number is:" + value3);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        private static void studentsgrade()
+        {
+            Console.Clear();
+            Console.WriteLine("Insert value for quiz score: ");
+            float value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert value for mid-term score: ");
+            float value2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert value for final score: ");
+            float value3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Scores: " + " Quiz: " + value1 + " Mid-Term: " + value2 + " Final: " + value3);
+
+            float average = (value1 + value2 + value3) / 3;
+
+            if (average >= 90)
+            {
+                Console.WriteLine("Final Score:" + average + " Grade A");
+            }
+            else if (average >= 70)
+            {
+                Console.WriteLine("Final Score:" + average + " Grade B");
+            }
+            else if (average >= 50)
+            {
+                Console.WriteLine("Final Score:" + average + " Grade C");
+            }
+            else
+            {
+                Console.WriteLine("Final Score:" + average + " Grade F");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void digits()
+        {
+            Console.Clear();
+            Console.WriteLine("Insert a value: ");
+            string value1 = Console.ReadLine();
+            Console.WriteLine(value1 + " Has a total of: " + value1.Length + "digits");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void ascii()
+        {
+            for (int i = 1; i < 122; i++)
+            {
+                Console.Write(" " + i + " = " + (char)i);
+
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(" ");
+
+                }
+
+            }
+        }
+
+        private static void spaces()
+        {
+            Console.Clear();
+            Console.WriteLine("Insert a value: ");
+            string value1 = Console.ReadLine();
+            var count = value1.Count(t => t == ' ');
+            Console.WriteLine("Count: " + count);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ New folder/exam practice/exam practice/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace exam_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Displaymenu = true;
+            while (Displaymenu)
+            {
+                Displaymenu = Option();
+           }
+        }
+
+
+
+        public static bool Option()
+        {
+            Console.WriteLine(" 1.greatest value of the three values.");
+            Console.WriteLine("2. Students Grade");
+            Console.WriteLine("3. Integer Number");
+            Console.WriteLine("4. ASCII VALUE");
+            Console.WriteLine("5. Spaces");
+            Console.WriteLine("Choose Option:");
+          
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Greater();
+                    return true;
+
+
+                case "2":
+                    Grade();
+                    return true;
+
+                case "3":
+                    integer();
+                    return true;
+                case "4":
+                    ascii();
+                    return true;
+                case "5":
+                    space();
+                    return true;
+
+                case "6":
+                    return false;
+                default:
+                    return true;
+
+            }
+        }
+
+        private static void Greater()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter First Value");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Value");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Value");
+            int value3 = Convert.ToInt32(Console.ReadLine());
+
+            if (value1 > value2 && value1 > value3)
+            { Console.WriteLine("Greater number is " + value1); }
+
+            else if (value2 > value1 && value2 > value3)
+            { Console.WriteLine("Greater number is" + value2); }
+
+            else if (value3 > value1 && value3 > value2)
+            { Console.WriteLine("Greater number is" + value3); }
+
+            else
+            { Console.WriteLine("All the value is same"); }
+
+
+        }
+
+        private static void Grade()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter Quiz Mark:");
+            float quiz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Mid Term Mark:");
+            float mid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Final Exam Mark:");
+            float final = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("total Mark:", quiz + mid + final);
+
+            float finalresult = (quiz + mid + final) / 3;
+            if (finalresult >= 90)
+            {
+                Console.WriteLine("Final Score:" + finalresult + " Grade A");
+            }
+            else if (finalresult >= 70)
+            {
+                Console.WriteLine("Final Score:" + finalresult + " Grade B");
+            }
+            else if (finalresult >= 50)
+            {
+                Console.WriteLine("Final Score:" + finalresult + " Grade C");
+            }
+            else
+            {
+                Console.WriteLine("Final Score:" + finalresult + " Grade F");
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+
+        private static void integer()
+        {
+            Console.Clear();
+            Console.WriteLine("Insert a value: ");
+            string value1 = Console.ReadLine();
+            Console.WriteLine(value1 + " Has a total of: " + value1.Length + "digits");
+
+
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        private static void ascii()
+        {
+            for (int i = 1; i < 122; i++)
+            {
+                Console.Write(" " + i + " = " + (char)i);
+
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(" ");
+
+                }
+
+            }
+
+        }
+
+        private static void space()
+        {
+          
+        }
+    }
+}
